7363ca117d9ff0f6c57d312e6d539b70bfc1dae7
Gregory Einfrank <greg.einfrank@venmo.com>
2014-01-19 14:01:35 -0500

    Trying to fix some stuff

--- emacs/dot_emacs_dot_d/el-get/.loaddefs.el
+++ emacs/dot_emacs_dot_d/el-get/.loaddefs.el
@@ -5,9 +5,9 @@
 
 ;;;### (autoloads (el-get-checksum el-get-make-recipes el-get-cd
 ;;;;;;  el-get-self-update el-get-update-packages-of-type el-get-update-all
-;;;;;;  el-get-version) "el-get/el-get" "el-get/el-get.el" (21212
-;;;;;;  4006 0 0))
-;;; Generated autoloads from el-get/el-get.el
+;;;;;;  el-get-version) "el-get/el-get" "../../../../.emacs.d/el-get/el-get/el-get.el"
+;;;;;;  "cb30b965a0a82be15c18d510be5de8d9")
+;;; Generated autoloads from ../../../../.emacs.d/el-get/el-get/el-get.el
 
 (autoload 'el-get-version "el-get/el-get" "\
 Message the current el-get version
@@ -48,8 +48,9 @@ Compute the checksum of the given package, and put it in the kill-ring
 ;;;***
 
 ;;;### (autoloads (el-get-list-packages) "el-get/el-get-list-packages"
-;;;;;;  "el-get/el-get-list-packages.el" (21212 4006 0 0))
-;;; Generated autoloads from el-get/el-get-list-packages.el
+;;;;;;  "../../../../.emacs.d/el-get/el-get/el-get-list-packages.el"
+;;;;;;  "0ef1653f1c95c87c4021d80e7a133e70")
+;;; Generated autoloads from ../../../../.emacs.d/el-get/el-get/el-get-list-packages.el
 
 (autoload 'el-get-list-packages "el-get/el-get-list-packages" "\
 Display a list of packages.
@@ -58,85 +59,15 @@ Display a list of packages.
 
 ;;;***
 
-;;;### (autoloads (pkg-info-version-info pkg-info-package-version
-;;;;;;  pkg-info-defining-library-version pkg-info-library-version)
-;;;;;;  "pkg-info/pkg-info" "../../../../.emacs.d/el-get/pkg-info/pkg-info.el"
-;;;;;;  "61d9a007b10af4401d2f97b73a75afa4")
-;;; Generated autoloads from ../../../../.emacs.d/el-get/pkg-info/pkg-info.el
-
-(autoload 'pkg-info-library-version "pkg-info/pkg-info" "\
-Get the version in the header of LIBRARY.
-
-LIBRARY is either a symbol denoting a named feature, or a library
-name as string.
-
-If SHOW is non-nil, show the version in the minibuffer.
-
-Return the version from the header of LIBRARY as list.  Signal an
-error if the LIBRARY was not found or had no proper header.
-
-See Info node `(elisp)Library Headers' for more information
-about library headers.
-
-\(fn LIBRARY &optional SHOW)" t nil)
-
-(autoload 'pkg-info-defining-library-version "pkg-info/pkg-info" "\
-Get the version of the library defining FUNCTION.
-
-If SHOW is non-nil, show the version in mini-buffer.
-
-This function is mainly intended to find the version of a major
-or minor mode, i.e.
-
-   (pkg-info-defining-library-version 'flycheck-mode)
-
-Return the version of the library defining FUNCTION (as by
-`pkg-info-locate-library-version').  Signal an error if FUNCTION
-is not a valid function, if its defining library was not found,
-or if the library had no proper version header.
-
-\(fn FUNCTION &optional SHOW)" t nil)
-
-(autoload 'pkg-info-package-version "pkg-info/pkg-info" "\
-Get the version of an installed PACKAGE.
-
-If SHOW is non-nil, show the version in the minibuffer.
-
-Return the version as list, or nil if PACKAGE is not installed.
-
-\(fn PACKAGE &optional SHOW)" t nil)
-
-(autoload 'pkg-info-version-info "pkg-info/pkg-info" "\
-Obtain complete version info for LIBRARY and PACKAGE.
-
-LIBRARY is a symbol denoting a named feature, or a library name
-as string.  PACKAGE is a symbol denoting an ELPA package.  If
-omitted or nil, default to LIBRARY.
-
-If SHOW is non-nil, show the version in the minibuffer.
-
-When called interactively, prompt for LIBRARY.  When called
-interactively with prefix argument, prompt for PACKAGE as well.
-
-Return a string with complete version information for LIBRARY.
-This version information contains the version from the headers of
-LIBRARY, and the version of the installed PACKAGE, the LIBRARY is
-part of.  If PACKAGE is not installed, or if the PACKAGE version
-is the same as the LIBRARY version, do not include a package
-version.
-
-\(fn LIBRARY &optional PACKAGE SHOW)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/el-get/dash/dash-functional.el"
-;;;;;;  "../../../../.emacs.d/el-get/dash/dash.el" "../../../../.emacs.d/el-get/epl/epl.el"
-;;;;;;  "../../../../.emacs.d/el-get/pkg-info/pkg-info.el" "../../../../.emacs.d/el-get/s/s.el"
-;;;;;;  "el-get/el-get-autoloads.el" "el-get/el-get-build.el" "el-get/el-get-byte-compile.el"
-;;;;;;  "el-get/el-get-core.el" "el-get/el-get-custom.el" "el-get/el-get-dependencies.el"
-;;;;;;  "el-get/el-get-install.el" "el-get/el-get-methods.el" "el-get/el-get-notify.el"
-;;;;;;  "el-get/el-get-recipes.el" "el-get/el-get-status.el") (21212
-;;;;;;  4567 172322 0))
+;;;### (autoloads nil nil ("../../../../.emacs.d/el-get/el-get/el-get-autoloads.el"
+;;;;;;  "../../../../.emacs.d/el-get/el-get/el-get-build.el" "../../../../.emacs.d/el-get/el-get/el-get-byte-compile.el"
+;;;;;;  "../../../../.emacs.d/el-get/el-get/el-get-core.el" "../../../../.emacs.d/el-get/el-get/el-get-custom.el"
+;;;;;;  "../../../../.emacs.d/el-get/el-get/el-get-dependencies.el"
+;;;;;;  "../../../../.emacs.d/el-get/el-get/el-get-install.el" "../../../../.emacs.d/el-get/el-get/el-get-list-packages.el"
+;;;;;;  "../../../../.emacs.d/el-get/el-get/el-get-methods.el" "../../../../.emacs.d/el-get/el-get/el-get-notify.el"
+;;;;;;  "../../../../.emacs.d/el-get/el-get/el-get-recipes.el" "../../../../.emacs.d/el-get/el-get/el-get-status.el"
+;;;;;;  "../../../../.emacs.d/el-get/el-get/el-get.el") (21212 8054
+;;;;;;  229227 0))
 
 ;;;***
 
