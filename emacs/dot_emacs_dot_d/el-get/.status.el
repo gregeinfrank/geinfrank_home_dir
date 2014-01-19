f8c9b17eae73bfb8a606203ad3072a50b41edf9d
Gregory Einfrank <greg.einfrank@venmo.com>
2014-01-19 12:57:46 -0500

    Adds projectile

--- emacs/dot_emacs_dot_d/el-get/.status.el
+++ emacs/dot_emacs_dot_d/el-get/.status.el
@@ -1,2 +1,15 @@
-((el-get status "installed" recipe
-	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :load "el-get.el")))
+((dash status "installed" recipe
+       (:name dash :description "A modern list api for Emacs. No 'cl required." :type github :pkgname "magnars/dash.el"))
+ (el-get status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
+ (epl status "installed" recipe
+      (:name epl :description "EPL provides a convenient high-level API for various package.el versions, and aims to overcome its most striking idiocies." :type github :pkgname "cask/epl"))
+ (pkg-info status "installed" recipe
+	   (:name pkg-info :description "Provide information about Emacs packages." :type github :pkgname "lunaryorn/pkg-info.el" :depends
+		  (dash epl)))
+ (projectile status "installed" recipe
+	     (:name projectile :description "Project navigation and management library for Emacs" :type github :pkgname "bbatsov/projectile" :depends
+		    (dash s pkg-info)
+		    :features projectile))
+ (s status "installed" recipe
+    (:name s :description "The long lost Emacs string manipulation library." :type github :pkgname "magnars/s.el" :features s)))
